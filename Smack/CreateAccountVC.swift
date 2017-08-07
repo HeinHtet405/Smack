9b32018749bbcacba55b52ab3159ded9dd0b0420
Hein Htet <heinhtet@Heins-iMac.local>
2017-08-07 16:07:59 +0630

    Lesson Six Commit

--- Smack/CreateAccountVC.swift
+++ Smack/CreateAccountVC.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
+    
+    
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func createAccountPressed(_sender: Any) {
+        guard let email = emailText.text, emailText.text != "" else {
+            return
+        }
+        guard let pass = passText.text, passText.text != "" else {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("register user!")
+            }
+        }
+        
+    }
+   
+    @IBAction func pickAvatarPressed(_sender: Any) {
+        
+    }
+    
+    @IBAction func pickBGColorPressed(_sender: Any) {
+        
+    }
+    
 
     @IBAction func closeBtnPressed(_ sender: Any) {
       performSegue(withIdentifier: UNWIND, sender: nil)
---
+++ Smack/RoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Hein Htet on 8/7/17.
+//  Copyright © 2017 Hein Htet. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
