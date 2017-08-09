f6d1a09515a352b639ddfbfa31bc84e50b023920
Hein Htet <heinhtet@Heins-iMac.local>
2017-08-09 16:46:36 +0630

    Complete Profile View

---
+++ Smack/ProfileVC.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileVC.swift
+//  Smack
+//
+//  Created by Hein Htet on 8/9/17.
+//  Copyright © 2017 Hein Htet. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func logoutBtnPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        userName.text = UserDataService.instance.name
+        email.text = UserDataService.instance.email
+        profileImage.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        
+        bgView.addGestureRecognizer(closeTouch)
+        
+    }
+    
+    func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+}
