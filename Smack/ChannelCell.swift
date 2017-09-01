731037791764455257524bca00f835fdd8d9e343
Hein Htet <heinhtet@Heins-iMac.local>
2017-09-01 15:36:47 +0630

    Finish Smack Chat But Continue to Develop remain api

--- Smack/ChannelCell.swift
+++ Smack/ChannelCell.swift
@@ -29,5 +29,13 @@ class ChannelCell: UITableViewCell {
     func configureCell(channel: Channel) {
         let title = channel.channelTitle ?? ""
         channelName.text = "#\(title)"
+        channelName.font = UIFont(name: "HelveticaNeue", size: 17)
+        
+        for id in MessageService.instance.unreadChannels {
+            if id == channel.id {
+              channelName.font = UIFont(name: "HelveticaNeue-Bold", size: 22)
+            }
+        }
+        
     }
 }
